01e69976cae4ab96c20a5c62f0f9f6cc4b26bad5
Taha <tahabarooti@yahoo.com>
2020-04-07 19:39:04 +0430

    Update languages.rb

--- config/initializers/languages.rb
+++ config/initializers/languages.rb
@@ -14,6 +14,6 @@ Rails.configuration.i18n.available_locales = configured_languages
 # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
 # the I18n.default_locale when a translation cannot be found).
 Rails.configuration.i18n.fallbacks = {}
-Rails.configuration.i18n.available_locales.each do |locale|
-  Rails.configuration.i18n.fallbacks[locale] = :en
+Rails.configuration.i18n.available_locales = [:fa_IR]
+  Rails.configuration.i18n.fallbacks[locale] = :fa_IR
 end
